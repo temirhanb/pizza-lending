2f3dca3a73eefb729769408d7e47004fc9e77cc4
temirhanb <temirhan023@gmaul.com>
2023-08-23 21:19:35 +0500

    - add popup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const applicantForm = document.getElementById('form')
+const popup = document.getElementById('popup'); // Фон попап окна
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
 
 const sendData = async (data) => {
   return await fetch('https://fakestoreapi.com/carts', {
@@ -22,8 +24,16 @@ async function handleFormSubmit(event) {
   event.preventDefault()
   const data = serializeForm(applicantForm)
   const response = await sendData(data)
-  if(response.status === 200) window.open('Заказ принят!')
+  if(response.status === 200) {
+    popup.classList.add('active');
+    popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+  }
   console.log(response)
 }
-
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+  if(e.target === popupBg) { // Если цель клика - фот, то:
+    popup.classList.remove('active'); // И с окна
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+  }
+});
 applicantForm.addEventListener('submit', handleFormSubmit)
