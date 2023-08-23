15859454658437e697e308addbd352b9e3899d02
temirhanb <temirhan023@gmaul.com>
2023-08-23 20:46:00 +0500

    - complete form api request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const applicantForm = document.getElementById('form')
 
-function handleFormSubmit(event) {
-  // Просим форму не отправлять данные самостоятельно
+const sendData = async (data) => {
+  return await fetch('https://fakestoreapi.com/carts', {
+    method: 'POST',
+    headers: {'Content-Type': 'multipart/form-data'},
+    body: data,
+  })
+}
+
+function serializeForm(formNode) {
+  const {elements} = formNode
+
+  return Array.from(elements)
+    .map((element) => {
+      const {name, value} = element
+      return {name, value}
+    })
+}
+
+async function handleFormSubmit(event) {
   event.preventDefault()
-  console.log('Отправка!')
+  const data = serializeForm(applicantForm)
+  const response = await sendData(data)
+  if(response.status === 200) window.open('Заказ принят!')
+  console.log(response)
 }
 
 applicantForm.addEventListener('submit', handleFormSubmit)
